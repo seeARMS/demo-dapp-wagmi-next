7b744789f6377691812aac79096b957afdcc8271
Colin Armstrong <colinarms@gmail.com>
2023-05-19 10:32:43 -0700

    minimal repro

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
-import '../styles/global.css';
-import type { AppProps } from 'next/app';
+import "../styles/global.css";
+import type { AppProps } from "next/app";
 
+import "@0xsequence/design-system/styles.css";
+import WagmiProvider from "components/WagmiProvider";
+import ThemeProvider from "components/ThemeProvider";
+import HomePage from "components/HomePage";
 
-import '@0xsequence/design-system/styles.css'
-import WagmiProvider from 'components/WagmiProvider';
-import ThemeProvider from 'components/ThemeProvider';
-import HomePage from 'components/HomePage'
+import { sequenceWallet } from "@0xsequence/rainbowkit-plugin";
+
+console.log(sequenceWallet);
 
 function MyApp() {
   return (
